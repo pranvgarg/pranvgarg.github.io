277444ef1fd463adb319a84c65973af16dac6a83
Pranav Garg <pranavgarg71@gmail.com>
2025-02-22 18:07:42 -0600

    Initial Push

---
+++ README.md
@@ -0,0 +1,1 @@
+# pranvgarg.github.io
---
+++ js/inline_scripts.js
@@ -0,0 +1,23 @@
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-0F2H6Z6HGL');
+  
+
+
+    emailScramble = new scrambledString(
+        document.getElementById('email'),
+        'emailScramble',
+        'mrago1ina@.rgapcagv7lm', // Updated scrambled email
+        [14, 1, 2, 6, 20, 11, 16, 3, 4, 12, 18, 8, 9, 7, 0, 19, 15, 13, 5, 10, 17, 21] // Updated indices
+    );
+    
+
+
+hideallbibs();
+
+
+// Original:  p r a n a v g a r g   7  1  @  g  m  a  i  l  .  c  o m
+// Indexes:   0 1 2 3 4 5 6 7 8 9  10 11 12 13 14 15 16 17 18 19 20 21
